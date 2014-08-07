e5f9ee8c6506d6a77db35bfe1b4699579007cf00
Jean Yves GASTAUD <jygastaud@gmail.com>
2014-08-07 22:10:15 +0200

    Change docker run default command.

--- settings/setup.sh
+++ settings/setup.sh
@@ -10,4 +10,4 @@ apt-get install curl -qq
 
 # Build first Docker with Wintersmith
 cd /vagrant && docker build -t jygastaud/wintersmith .
-docker run -p 49160:8080 -d jygastaud/wintersmith
+docker run -v ${PWD}/www/site:/www/site -p 49160:8080 -d jygastaud/wintersmith
